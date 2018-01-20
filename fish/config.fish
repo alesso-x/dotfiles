b738ce8857f4f89a378cbefd37df5bcb32fb71f1
Alessandro DiMarco <axdimarco@gmail.com>
2018-01-19 22:34:56 -0500

    Add alias for docker-compose

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,5 @@ status --is-interactive; and source (nodenv init -|psub)
 
 # aws cli completion
 test -x (which aws_completer); and complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
+
+alias dk "docker-compose"
