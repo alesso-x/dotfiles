753ce45f6fbe3d639d903cdcb7f0983e7022c50f
Alessandro DiMarco <axdimarco@gmail.com>
2018-10-23 12:39:44 -0400

    Add comments to fish shell functions

--- fish/functions/posix-source.fish
+++ fish/functions/posix-source.fish
@@ -1,3 +1,5 @@
+# Load environment variables from file
+# From http://lewandowski.io/2016/10/fish-env/
 function posix-source
     for i in (cat $argv)
         set arr (echo $i |tr = \n)
--- fish/functions/pull-repos.fish
+++ fish/functions/pull-repos.fish
@@ -1,3 +1,4 @@
+# Delete merged branches, switch branch to master, git pull all repos in current working directory
 function pull-repos
     find . -type d -depth 1 -exec git --git-dir='{}'/.git --work-tree=$PWD/'{}' delete-merged-branches \;
 end
