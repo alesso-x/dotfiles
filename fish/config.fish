6488ce3bcf5857e3e2022944d0826e326295b185
Alessandro DiMarco <axdimarco@gmail.com>
2019-02-10 19:38:02 -0500

    Update path set

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-set -g -x PATH $PATH /Users/alesso/.local/bin
+set -gx PATH $PATH /Users/alesso/.local/bin
 
 # aws cli completion
 test -x (which aws_completer); and complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
