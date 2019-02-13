000a5bb074008b40911da9c48fb44bf23853b49d
Alessandro DiMarco <axdimarco@gmail.com>
2019-02-12 21:53:40 -0500

    rearrange config file

--- fish/config.fish
+++ fish/config.fish
@@ -1,19 +1,17 @@
-# coreutils, gnu-sed, hatch
-set -gx PATH /usr/local/opt/coreutils/libexec/gnubin /usr/local/opt/gnu-sed/libexec/gnubin $PATH /Users/alesso/.local/bin
-
-# aws cli completion
-test -x (which aws_completer); and complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
-
+eval (direnv hook fish)
 alias d "docker"
 alias dm "docker-compose"
 alias k "kubectl"
 
-source /usr/local/opt/asdf/asdf.fish
+# coreutils, gnu-sed, hatch
+set -x PATH /usr/local/opt/coreutils/libexec/gnubin /usr/local/opt/gnu-sed/libexec/gnubin $PATH /Users/alesso/.local/bin
+
+# aws cli completion
+test -x (which aws_completer); and complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
 
 # django
 __fish_complete_django django-admin.py
 __fish_complete_django manage.py
 
-eval (direnv hook fish)
-
+source /usr/local/opt/asdf/asdf.fish
 source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
