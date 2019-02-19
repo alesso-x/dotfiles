47f8abe51a60120fef86219c0e74673a26850b79
Alessandro DiMarco <axdimarco@gmail.com>
2019-02-19 17:54:39 -0500

    Add gnu getopt for gcloud

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,8 +3,8 @@ alias d "docker"
 alias dm "docker-compose"
 alias k "kubectl"
 
-# coreutils, gnu-sed, hatch
-set -x PATH /usr/local/opt/coreutils/libexec/gnubin /usr/local/opt/gnu-sed/libexec/gnubin $PATH /Users/alesso/.local/bin
+# coreutils, gnu-sed, gnu-getopt, hatch
+set -x PATH /usr/local/opt/coreutils/libexec/gnubin /usr/local/opt/gnu-getopt/bin /usr/local/opt/gnu-sed/libexec/gnubin $PATH /Users/alesso/.local/bin
 
 # aws cli completion
 test -x (which aws_completer); and complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
