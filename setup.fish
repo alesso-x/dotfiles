bfe14016c673fc4ca0fdd519c29e88b8c5bafbf2
Alessandro DiMarco <axdimarco@gmail.com>
2019-03-13 15:50:27 -0400

    Remove fisher

--- setup.fish
+++ setup.fish
@@ -1,11 +1,5 @@
 #!/usr/bin/env fish
 
-echo "Installing fisher"
-curl https://git.io/fisher --create-dirs -sLo $HOME/.config/fish/functions/fisher.fish
-
-echo "Installing plugins"
-fisher
-
 echo "Install omf"
 curl -L https://get.oh-my.fish | fish
 
