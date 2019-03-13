a64d6367d98323a2f64c1d32e1548cb67db036e4
Alessandro DiMarco <axdimarco@gmail.com>
2019-03-13 11:37:11 -0400

    Move source commands to the top

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,6 @@
+fenv source ~/dotfiles/.environment_vars
+bass source ~/dotfiles/.aliases
+
 # path
 set -x PATH /usr/local/opt/coreutils/libexec/gnubin /usr/local/opt/gnu-getopt/bin /usr/local/opt/gnu-sed/libexec/gnubin $PATH /Users/alesso/.local/bin /usr/local/sbin
 
@@ -11,5 +14,3 @@ source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.in
 __fish_complete_django django-admin.py
 __fish_complete_django manage.py
 
-fenv source ~/dotfiles/.environment_vars
-bass source ~/dotfiles/.aliases
