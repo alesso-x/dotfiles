f50957066da9e1c6a4c84ed6559d93d07b975b24
Alessandro DiMarco <axdimarco@gmail.com>
2019-03-27 13:44:36 -0400

    Add cheat.sh to config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -18,11 +18,4 @@ source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.in
 __fish_complete_django django-admin.py
 __fish_complete_django manage.py
 
-# retrieve command cheat sheets from cheat.sh
-# fish version by @tobiasreischmann
-function cheat.sh
-    curl cheat.sh/$argv
-end
-
-# register completions (on-the-fly, non-cached, because the actual command won't be cached anyway
-complete -c cheat.sh -xa '(curl -s cheat.sh/:list)'
+complete -c cht.sh -xa '(curl -s cheat.sh/:list)'
--- install.sh
+++ install.sh
@@ -37,6 +37,12 @@ brew bundle --file=./macOS/Brewfile
 brew cleanup
 
 #
+# Cheat.sh
+#
+curl https://cht.sh/:cht.sh | sudo tee /usr/local/bin/cht.sh
+chmod +x /usr/local/bin/cht.sh
+
+#
 # Fish setup
 #
 
