3e432abea84d095faa3e296a7e282f81a5783afe
Alessandro DiMarco <axdimarco@gmail.com>
2019-06-06 13:45:24 -0400

    Update dock

--- macOS/dock.sh
+++ macOS/dock.sh
@@ -4,18 +4,18 @@ dockutil --add /Applications/Launchpad.app
 dockutil --add /Applications/Safari.app
 dockutil --add /Applications/Firefox.app
 dockutil --add /Applications/Google\ Chrome.app
+dockutil --add /Applications/iTunes.app
 dockutil --add /Applications/Mail.app
 dockutil --add /Applications/Calendar.app
-dockutil --add /Applications/Notes.app
-dockutil --add /Applications/Reminders.app
-dockutil --add /Applications/Slack.app
 dockutil --add /Applications/News.app
+dockutil --add /Applications/Reeder.app
 dockutil --add /Applications/Messages.app
 dockutil --add /Applications/WhatsApp.app
-dockutil --add /Applications/iTunes.app
+dockutil --add /Applications/Signal.app
+dockutil --add /Applications/Slack.app
+dockutil --add /Applications/Bear.app
 dockutil --add /Applications/iTerm.app
 dockutil --add /Applications/Visual\ Studio\ Code.app
-dockutil --add /Applications/Quiver.app
 dockutil --add /Applications/DataGrip.app
 dockutil --add "$HOME/Library/Mobile Documents/com~apple~CloudDocs/screenshots" --view grid --display stack --sort dateadded
 dockutil --add "$HOME/Downloads" --view grid --display stack --sort dateadded
