dc70249318720fbadfa17675ff0c03435b8d0d77
Alessandro DiMarco <alesso@Alessos-MacBook-Pro-199.local>
2019-06-17 23:28:10 -0400

    Update paths of dock apps

--- macOS/dock.sh
+++ macOS/dock.sh
@@ -1,20 +1,20 @@
 dockutil --remove all
 
-dockutil --add /Applications/Launchpad.app
+dockutil --add /System/Applications/Launchpad.app
 dockutil --add /Applications/Safari.app
 dockutil --add /Applications/Firefox.app
 dockutil --add /Applications/Google\ Chrome.app
-dockutil --add /Applications/Mail.app
-dockutil --add /Applications/Calendar.app
-dockutil --add /Applications/Messages.app
+dockutil --add /System/Applications/Mail.app
+dockutil --add /System/Applications/Calendar.app
+dockutil --add /System/Applications/Messages.app
 dockutil --add /Applications/WhatsApp.app
 dockutil --add /Applications/Slack.app
-dockutil --add /Applications/News.app
-dockutil --add /Applications/Music.app
+dockutil --add /System/Applications/News.app
+dockutil --add /System/Applications/Music.app
 dockutil --add /Applications/Bear.app
 dockutil --add /Applications/iTerm.app
 dockutil --add /Applications/Visual\ Studio\ Code.app
 dockutil --add /Applications/Xcode.app
 dockutil --add /Applications/DataGrip.app
-dockutil --add "$HOME/Library/Mobile Documents/com~apple~CloudDocs/screenshots" --view grid --display stack --sort dateadded
+dockutil --add "$HOME/Library/CloudStorage/iCloud Drive/screenshots" --view grid --display stack --sort dateadded
 dockutil --add "$HOME/Downloads" --view grid --display stack --sort dateadded
