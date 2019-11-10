de16a6043adc4c0a7a5f358c77954f4414dba46b
Alessandro DiMarco <axdimarco@gmail.com>
2019-11-10 16:48:31 -0500

    Update dock.sh

--- macOS/dock.sh
+++ macOS/dock.sh
@@ -2,8 +2,8 @@ dockutil --remove all
 
 dockutil --add /Applications/Safari.app
 dockutil --add /Applications/Firefox.app
-dockutil --add /Applications/Google\ Chrome.app
-dockutil --add /Applications/Microsoft\ Outlook.app
+dockutil --add /System/Applications/Mail.app
+dockutil --add /System/Applications/Calendar.app
 dockutil --add /System/Applications/Messages.app
 dockutil --add /Applications/WhatsApp.app
 dockutil --add /Applications/Slack.app
@@ -14,6 +14,6 @@ dockutil --add /Applications/iTerm.app
 dockutil --add /Applications/Visual\ Studio\ Code.app
 dockutil --add /Applications/Xcode.app
 dockutil --add /Applications/DataGrip.app
+dockutil --add /Applications/Insomnia.app
 dockutil --add "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Screenshots" --view grid --display stack --sort dateadded
-# dockutil --add "$HOME/Library/CloudStorage/iCloud Drive/Screenshots" --view grid --display stack --sort dateadded
 dockutil --add "$HOME/Downloads" --view grid --display stack --sort dateadded
