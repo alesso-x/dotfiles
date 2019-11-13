4fea9f7f24b0a4f2e1f60c2957f12e8b7cfbbb40
Alessandro DiMarco <axdimarco@gmail.com>
2019-11-13 17:31:17 -0500

    Add comment

--- .environment_vars.sh
+++ .environment_vars.sh
@@ -2,6 +2,8 @@ set -a
 
 # XDG
 # https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
+# https://golang.org/pkg/os/#UserConfigDir
+#
                                   # Darwin
 XDG_CONFIG_HOME=$HOME/.config     # $HOME/Library/Preferences (helm)  $HOME/Library/Application Support (doctl)
 XDG_DATA_HOME=$HOME/.local/share  # $HOME/Library
