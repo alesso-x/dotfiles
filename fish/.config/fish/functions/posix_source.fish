2a13c472c16246d63ee40d9e78484ff4602c785e
Alessandro DiMarco <axdimarco@gmail.com>
2019-11-17 20:24:49 -0500

    Fix posix source naming convention

--- fish/.config/fish/functions/posix_source.fish
+++ fish/.config/fish/functions/posix_source.fish
@@ -1,6 +1,6 @@
 # Load environment variables from file
 # From http://lewandowski.io/2016/10/fish-env/
-function posix-source
+function posix_source
     for i in (cat $argv)
         set arr (echo $i |tr = \n)
         set -gx $arr[1] $arr[2]
