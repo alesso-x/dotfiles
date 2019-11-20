289a5e49a5e8c150a58d1e26773cf92b8dec2304
Alessandro DiMarco <axdimarco@gmail.com>
2019-11-19 22:12:55 -0500

    Add alias ll

--- .aliases.sh
+++ .aliases.sh
@@ -1,2 +1,3 @@
 alias ls='/usr/local/bin/gls --color -h --group-directories-first'
+alias ll='/usr/local/bin/gls --color -h --group-directories-first -la'
 alias pull-repos='find . -type d -depth 1 -print -exec git --git-dir={}/.git --work-tree=$PWD/{} delete-merged \;'
