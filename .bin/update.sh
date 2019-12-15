2db09ad6a7741a1fc97ea4488016bb8f5709c267
Alessandro DiMarco <axdimarco@gmail.com>
2019-12-15 16:49:36 -0500

    Update brewfile

--- .bin/update.sh
+++ .bin/update.sh
@@ -1,2 +1,2 @@
 code --list-extensions > $HOME/dotfiles/vscode/extensions
-brew bundle dump --force --file=$HOME/dotfiles/macOS/Brewfile
+brew bundle dump --force --no-restart --file=$HOME/dotfiles/macOS/Brewfile
