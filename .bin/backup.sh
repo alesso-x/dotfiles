993d75144fd852d9a089d754cea21f14a7d74922
Alessandro DiMarco <axdimarco@gmail.com>
2020-02-03 22:45:43 -0500

    Add shebang to bash scripts

--- .bin/backup.sh
+++ .bin/backup.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 cp $HOME/.local/share/fish/fish_history $HOME/dotfiles/backup/
--- .bin/update.sh
+++ .bin/update.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 code --list-extensions > $HOME/dotfiles/vscode/extensions
 brew bundle dump --force --no-restart --file=$HOME/dotfiles/macOS/Brewfile
