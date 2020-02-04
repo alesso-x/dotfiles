508008443b5b5e29b8208daff0b266ad829c3312
Alessandro DiMarco <axdimarco@gmail.com>
2020-02-03 22:45:29 -0500

    Add dotfiles func

---
+++ .bin/addFile.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#######################################
+# Add file to dotfiles
+#
+# Globals:
+#   None
+# Arguments:
+#   folder - folder for file path in dotfiles
+#   path - file path to add
+# Returns:
+#   None
+#######################################
+
+# Example
+#
+# Add a file
+# ./.bin/addFile.sh vim ~/.vimrc
+#
+# Remove symlink
+# stow --delete vim
+
+folder=$1
+path=$2
+
+if [[ ! "$path" == *"$HOME"* ]]; then
+  echo "File path must be absolute from home directory"
+  exit 1
+fi
+
+dotfile_path=$HOME/dotfiles/$folder/${path#$HOME/}
+echo "Adding $path to dotfiles in $folder"
+mkdir -p $(dirname "$dotfile_path"); touch $dotfile_path
+stow --adopt $folder
