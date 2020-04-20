a6805550d1b9388f62ba40956788ef9e5652bc6c
Alessandro DiMarco <axdimarco@gmail.com>
2020-04-20 17:11:07 -0400

    update dj with environment variables auto completino

--- fish/.config/fish/functions/dj.fish
+++ fish/.config/fish/functions/dj.fish
@@ -1,3 +1,5 @@
-function dj  --wraps docker-compose
-    genv -C $HOME/workspace/dev/ docker-compose $argv
+function dj --wraps docker-compose
+    # export environment variables to hide docker-compose env warnings
+    export (cat $HOME/workspace/dev/.env)
+    genv -C $HOME/workspace/dev/ docker-compose --env-file $HOME/workspace/dev/.env $argv
 end
