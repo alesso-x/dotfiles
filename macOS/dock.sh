45fc4b6f4e3d3321290563cde296c8c2c358ef20
Alessandro DiMarco <8059120+alesso-x@users.noreply.github.com>
2020-08-21 11:13:39 -0400

    update dock

--- macOS/dock.sh
+++ macOS/dock.sh
@@ -1,6 +1,8 @@
 dockutil --remove all
 
+dockutil --add /System/Applications/Launchpad.app
 dockutil --add /Applications/Safari.app
+dockutil --add /Applications/Google\ Chrome.app
 dockutil --add /Applications/Firefox.app
 dockutil --add /System/Applications/Mail.app
 dockutil --add /System/Applications/Calendar.app
@@ -8,13 +10,12 @@ dockutil --add /System/Applications/Messages.app
 dockutil --add /Applications/WhatsApp.app
 dockutil --add /Applications/Discord.app
 dockutil --add /Applications/Slack.app
-dockutil --add /System/Applications/News.app
 dockutil --add /System/Applications/Music.app
+dockutil --add /Applications/Poolside\ FM.app
 dockutil --add /Applications/Bear.app
 dockutil --add /Applications/iTerm.app
 dockutil --add /Applications/Visual\ Studio\ Code.app
 dockutil --add /Applications/Xcode.app
 dockutil --add /Applications/DataGrip.app
-dockutil --add /Applications/Insomnia.app
 dockutil --add "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Screenshots" --view grid --display stack --sort dateadded
 dockutil --add "$HOME/Downloads" --view grid --display stack --sort dateadded
