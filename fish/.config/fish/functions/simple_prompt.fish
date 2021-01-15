42fc2b39f6032daf12e4ea499f044e616d6a16a2
Alessandro DiMarco <8059120+alesso-x@users.noreply.github.com>
2021-01-14 19:32:24 -0500

    break out set_get_colors for prompt

--- fish/.config/fish/functions/simple_prompt.fish
+++ fish/.config/fish/functions/simple_prompt.fish
@@ -1,6 +1,45 @@
 function simple_prompt
     set -l last_status $status
 
+    set_git_colors
+
+    if not set -q __fish_prompt_normal
+        set -g __fish_prompt_normal (set_color normal)
+    end
+
+    set -l color_cwd
+    set -l prefix
+    set -l suffix
+    switch "$USER"
+        case root toor
+            if set -q fish_color_cwd_root
+                set color_cwd $fish_color_cwd_root
+            else
+                set color_cwd $fish_color_cwd
+            end
+            set suffix '#'
+        case '*'
+            set color_cwd $fish_color_cwd
+            set suffix (set_color red)'❯'
+    end
+
+    # PWD
+    set_color $color_cwd
+    echo -n (prompt_pwd)
+    set_color normal
+
+    printf '%s ' (__fish_vcs_prompt)
+
+    if not test $last_status -eq 0
+        set_color $fish_color_error
+    end
+
+    echo -n "$suffix "
+
+    set_color normal
+end
+
+function set_git_colors
     if not set -q __fish_git_prompt_show_informative_status
         set -g __fish_git_prompt_show_informative_status 1
     end
@@ -55,39 +94,4 @@ function simple_prompt
     if not set -q __fish_git_prompt_color_cleanstate
         set -g __fish_git_prompt_color_cleanstate green --bold
     end
-
-    if not set -q __fish_prompt_normal
-        set -g __fish_prompt_normal (set_color normal)
-    end
-
-    set -l color_cwd
-    set -l prefix
-    set -l suffix
-    switch "$USER"
-        case root toor
-            if set -q fish_color_cwd_root
-                set color_cwd $fish_color_cwd_root
-            else
-                set color_cwd $fish_color_cwd
-            end
-            set suffix '#'
-        case '*'
-            set color_cwd $fish_color_cwd
-            set suffix (set_color red)'❯'
-    end
-
-    # PWD
-    set_color $color_cwd
-    echo -n (prompt_pwd)
-    set_color normal
-
-    printf '%s ' (__fish_vcs_prompt)
-
-    if not test $last_status -eq 0
-        set_color $fish_color_error
-    end
-
-    echo -n "$suffix "
-
-    set_color normal
 end
