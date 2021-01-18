02b0ed3b95cecf2da2fbcdb98aef2f57a9027140
Alessandro DiMarco <8059120+alesso-x@users.noreply.github.com>
2021-01-18 10:18:32 -0500

    add todo

--- fish/.config/fish/functions/dj.fish
+++ fish/.config/fish/functions/dj.fish
@@ -1,5 +1,6 @@
 function dj --wraps docker-compose
     # export environment variables to hide docker-compose env warnings
     export (cat $HOME/workspace/dev/.env)
+    # TODO: check for arg "upd" and use the USE_CONTAINERS
     genv -C $HOME/workspace/dev/ docker-compose --env-file $HOME/workspace/dev/.env $argv
 end
