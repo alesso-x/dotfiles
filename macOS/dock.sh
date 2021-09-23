a873a01d336ab0e6cdd4b38faccf99ee6717a6cb
Alessandro DiMarco <8059120+alesso-x@users.noreply.github.com>
2021-09-23 00:43:06 -0400

    update dock and replace icons

--- macOS/dock.sh
+++ macOS/dock.sh
@@ -2,18 +2,15 @@ dockutil --remove all
 
 dockutil --add /System/Applications/Launchpad.app
 dockutil --add /Applications/Safari.app
-dockutil --add /Applications/Google\ Chrome.app
 dockutil --add /System/Applications/Mail.app
 dockutil --add /System/Applications/Calendar.app
 dockutil --add /System/Applications/Messages.app
 dockutil --add /Applications/WhatsApp.app
-dockutil --add /Applications/Signal.app
-dockutil --add /Applications/Discord.app
 dockutil --add /Applications/Slack.app
 dockutil --add /Applications/Twitter.app
 dockutil --add /System/Applications/Music.app
 dockutil --add /Applications/Spotify.app
-dockutil --add /Applications/Poolside\ FM.app
+dockutil --add /Applications/Poolsuite\ FM.app
 dockutil --add /Applications/DaftCloud.app
 dockutil --add /Applications/Bear.app
 dockutil --add /Applications/Reeder.localized/Reeder.app
@@ -21,5 +18,6 @@ dockutil --add /Applications/iTerm.app
 dockutil --add /Applications/Visual\ Studio\ Code.app
 dockutil --add /Applications/DataGrip.app
 dockutil --add /Applications/Insomnia.app
+dockutil --add /Applications/Lens.app
 dockutil --add "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Screenshots" --view grid --display stack --sort dateadded
 dockutil --add "$HOME/Downloads" --view grid --display stack --sort dateadded
