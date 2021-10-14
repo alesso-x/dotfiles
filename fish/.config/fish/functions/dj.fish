5d40540cf138683b2ab59ed6966d384e0a24eb55
Alessandro DiMarco <8059120+alesso-x@users.noreply.github.com>
2021-10-13 23:04:54 -0400

    Remove dj fish function

--- fish/.config/fish/functions/dj.fish
+++
@@ -1,4 +0,0 @@
-function dj --wraps docker-compose
-    # TODO: check for arg "upd" and use the USE_CONTAINERS
-    genv -C $HOME/workspace/dev/ docker-compose --env-file $HOME/workspace/dev/.env $argv
-end
